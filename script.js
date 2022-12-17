9f5cf04b362fe06d87e0d48b4ffbeb50c8027742
Alan Pason <alan.pason132@gmail.com>
2022-12-17 13:16:03 +0100

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,18 @@
+// uzytkownik wybiera jedna z 3 opcji
+// porownuje ja z komputerem
+// jesli uzytkownik wygrywa dostaje o tym komunikat ze wygral
+// jesli uzytkownik przegrywa dostaje o tym komunikat ze przegral
+// jesli uzytkownik remisuje dostaje o tym komunikat ze zremisowal
+
+//crate an array with 3 options to randomize
+// uzytkownik wybiera jedna z 3 opcji i przypisuje do zmiennej
+// computer randomize one of 3 options and assigns to variable
+// then contiunes comparsion and
+//     if users wins
+//        then annoucment that he wins
+//     else if computer wins
+//        then annouccment that computer wins
+//     else if draws
+//        then annoucment that is draws
+//     else if users put something diffrent than one of 3 options
+//        then annoucment that he must put diffrent variable
