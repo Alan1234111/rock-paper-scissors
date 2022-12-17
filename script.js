8a5c81e58a8cd9269e294fac390b03f7d1e0104c
Alan Pason <alan.pason132@gmail.com>
2022-12-17 13:39:27 +0100

    Rock Paper Scissors in console

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // uzytkownik wybiera jedna z 3 opcji
 // porownuje ja z komputerem
+// jesli uzytkownik wybral kamien wygrywa z nozyczkami ale przegrywa z papierem
+// jesli uzytkownik wybral nozyczki wygrywa z papierem ale przegrywa z kamieniem
+//jesli uzytkownik wybral papier wygrywa z kamieniem ale przegrywa z nozyczkami
 // jesli uzytkownik wygrywa dostaje o tym komunikat ze wygral
 // jesli uzytkownik przegrywa dostaje o tym komunikat ze przegral
 // jesli uzytkownik remisuje dostaje o tym komunikat ze zremisowal
@@ -16,3 +19,34 @@
 //        then annoucment that is draws
 //     else if users put something diffrent than one of 3 options
 //        then annoucment that he must put diffrent variable
+
+const options = ["rock", "paper", "scissors"];
+
+function chooseRandomOption() {
+  return options[Math.floor(Math.random() * 3)];
+}
+
+const playerChoice = prompt("What is your choice (rock, paper, scissors)");
+const computerChoice = chooseRandomOption();
+
+function whoWins(playerChoice, computerChoice) {
+  if (
+    (playerChoice.toLowerCase() == "rock" && computerChoice == "scissors") ||
+    (playerChoice.toLowerCase() == "scissors" && computerChoice == "paper") ||
+    (playerChoice == "paper" && computerChoice == "rock")
+  ) {
+    console.log("Congratulations YOU WIN!");
+  } else if (
+    (playerChoice.toLowerCase() == "rock" && computerChoice == "paper") ||
+    (playerChoice.toLowerCase() == "scissors" && computerChoice == "rock") ||
+    (playerChoice == "paper" && computerChoice == "scissors")
+  ) {
+    console.log("YOU LOSE");
+  } else if (playerChoice.toLowerCase() == computerChoice) {
+    console.log("DRAW");
+  } else {
+    console.log("You must put 1 of 3 options");
+  }
+}
+
+whoWins(playerChoice, computerChoice);
